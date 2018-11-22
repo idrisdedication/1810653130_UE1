a4eb7dd79313593ae25eaf97416e04a70e33afec
unknown <you@example.com>
2018-11-22 02:14:16 +0100

    Übung 2 fertig

---
+++ src/Aufgabe2_1810653130.java
@@ -0,0 +1,19 @@
+public class Aufgabe2_1810653130 {
+    public static void main(String[] args)  // Main Methode
+    {
+        String name = "Idris Atalay";       // Wertebereich Zeichenanreihungen
+        byte b = -100;                      // Wertebereich -128 bis 127 => Ganzzahl
+        short c = 30;                       // Wertebereich -32,768 bis 32,767 => Ganzzahl
+        int age = 21;                       // Wertebereich -2,147,483,648 bis 2,147,483,647 =>Ganzzahl
+        long a = 60;                        // Wertebereich -9,223,372,036,854,775,808 bis 9,223,372,036,854,775,807 => Ganzzahl
+        float d = 8;                        // Wertebereich ca. 1.4 x 10^45 bis 3.4 x 10^38 => Fließkommazahl
+        double exactAge = 21.25;            // Wertebereich ca. 4.9 x 10^-324 bis 1.7 x 10^308 => Fließkommazahl
+        boolean IdrisHatRecht = true;       // Wertebereich true/false => Wahrheitswert
+        char Zeichen = 'I';                 // Wertebereich 65,536 unterschiedliche Zeichen => Zeichen
+        int zahl  = 218;                    // Unterschied zwischen Variable und Konstante:
+
+        // Konstante
+        final int i = 126;                  // Final; Konstanten legen einen unveränderbaren Grenzwert fest.
+                                            // <-> Variablen können nachträglich noch geändert werden.
+    }
+}
