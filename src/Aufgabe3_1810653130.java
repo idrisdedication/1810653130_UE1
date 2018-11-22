ba41320497411e53e72faff96fb89714d44fb070
unknown <you@example.com>
2018-11-22 02:41:28 +0100

    Übung 3 fertig

---
+++ src/Aufgabe3_1810653130.java
@@ -0,0 +1,19 @@
+public class Aufgabe3_1810653130 {
+    public static void main(String[] args)
+    {
+        int zahl1 = 1;
+        int zahl2 = 2;
+        int zahl3 = 3;
+
+        double zahl4 = 4.5;
+        double zahl5 = 5.5;
+        double zahl6 = 6.5;
+
+        System.out.println(zahl1 + zahl2);
+        System.out.println(zahl1 / zahl3);
+        System.out.println(zahl4 * zahl2);
+        System.out.println(zahl1 % zahl2);
+        System.out.println(++zahl3);
+        System.out.println(--zahl2);
+    }
+}
---
+++ src/Aufgabe4_1810653130.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+
+public class Aufgabe4_1810653130 {
+
+    public static void main(String[]args)
+    {
+        Integer zahl1 = 5;
+        Integer zahl2 = 10;
+
+        System.out.println(zahl1 < zahl2);
+        System.out.println(zahl2 < zahl1);
+        System.out.println(zahl1 > zahl2);
+        System.out.println(zahl2 > zahl1);
+        System.out.println(zahl1 <= zahl2);
+        System.out.println(zahl2 <= zahl1);
+        System.out.println(zahl1 >= zahl2);
+        System.out.println(zahl2 >= zahl1);
+        System.out.println(zahl1 == zahl2);
+        System.out.println(zahl1 != zahl2);
+    }
+}
